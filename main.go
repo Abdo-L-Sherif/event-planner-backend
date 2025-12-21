04b2f29baab96c2cad358b5f7a384eb0262c137c
0marAmara <onan383@outlook.com>
2025-12-21 15:18:52 +0200

    final version

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	// 1. Fixed the "DELET" typo to "DELETE"
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
